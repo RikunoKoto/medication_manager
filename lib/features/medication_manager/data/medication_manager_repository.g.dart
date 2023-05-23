8066befa7b0a8973ed1974ca4ba18b9d2aa47861
MorishitaRyota <83997707+MorishitaRyota@users.noreply.github.com>
2023-05-23 16:59:50 +0900

    change

---
+++ lib/features/medication_manager/data/medication_manager_repository.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'medication_manager_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$medicationIsarHash() => r'2d2afe6fb8a3d8e6de829e3c829a13f37eb2f262';
+
+/// See also [medicationIsar].
+@ProviderFor(medicationIsar)
+final medicationIsarProvider = AutoDisposeProvider<Isar>.internal(
+  medicationIsar,
+  name: r'medicationIsarProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$medicationIsarHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MedicationIsarRef = AutoDisposeProviderRef<Isar>;
+String _$medicationManagerRepositoryHash() =>
+    r'ab02c6ddb8625148b7e8bfdeee93ef13779611f9';
+
+/// See also [medicationManagerRepository].
+@ProviderFor(medicationManagerRepository)
+final medicationManagerRepositoryProvider =
+    AutoDisposeProvider<MedicationManagerRepository>.internal(
+  medicationManagerRepository,
+  name: r'medicationManagerRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$medicationManagerRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MedicationManagerRepositoryRef
+    = AutoDisposeProviderRef<MedicationManagerRepository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ lib/features/medication_manager/domain/medication_item.dart
@@ -0,0 +1,41 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'medication_item.freezed.dart';
+
+@freezed
+class MedicationItem with _$MedicationItem {
+  const factory MedicationItem({
+    required int id,
+
+    ///　薬の名前
+    required String name,
+
+    /// 服用回数
+    required int dosageFrequency,
+
+    /// 用量（ex.２錠）
+    required int dosage,
+
+    /// 服用期間
+    required DateTime dosingPeriod,
+
+    ///　期間終了フラグ
+    @Default(false) bool isCompleted,
+  }) = _MedicationItem;
+  const MedicationItem._();
+
+  MedicationItem update({
+    required String name,
+    required int dosageFrequency,
+    required int dosage,
+    required DateTime dosingPeriod,
+  }) =>
+      copyWith(
+        name: name,
+        dosageFrequency: dosageFrequency,
+        dosage: dosage,
+        dosingPeriod: dosingPeriod,
+      );
+
+  bool get isEmptyName => name.isEmpty;
+}
